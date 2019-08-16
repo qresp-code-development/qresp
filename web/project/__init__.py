4dd0407534a2ba65baada56184ab3d65ad445cbe
Raul Laasner <raullaasner@gmail.com>
2019-08-16 11:36:23 -0400

    Use python-decouple for environment variables

--- web/project/__init__.py
+++ web/project/__init__.py
@@ -1,3 +1,4 @@
+from decouple import config
 import os
 
 from flask import Flask
@@ -6,9 +7,14 @@ from flask_wtf import CSRFProtect
 from flask_sitemap import Sitemap
 import connexion
 
-GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
-GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET")
-REDIRECT_URI = os.environ.get("REDIRECT_URI")
+HYBRID3_URL = config('HYBRID3_URL', default='')
+HOST_URL = config('HOST_URL', default='')
+
+GOOGLE_CLIENT_ID = config('GOOGLE_CLIENT_ID',
+                          default=os.environ.get("GOOGLE_CLIENT_ID"))
+GOOGLE_CLIENT_SECRET = config('GOOGLE_CLIENT_SECRET',
+                              default=os.environ.get("GOOGLE_CLIENT_SECRET"))
+REDIRECT_URI = config('REDIRECT_URI', default=os.environ.get("REDIRECT_URI"))
 MAIL_ADDR = os.environ.get("MAIL_ADDR")
 MAIL_PWD = os.environ.get("MAIL_PWD")
 
@@ -16,11 +22,13 @@ AUTH_URI = 'https://accounts.google.com/o/oauth2/auth'
 TOKEN_URI = 'https://accounts.google.com/o/oauth2/token'
 USER_INFO = 'https://www.googleapis.com/userinfo/v2/me'
 
-MONGODB_HOST = os.environ.get("MONGODB_HOST")
-MONGODB_PORT = os.environ.get("MONGODB_PORT")
-MONGODB_USERNAME = os.environ.get("MONGODB_USERNAME")
-MONGODB_PASSWORD = os.environ.get("MONGODB_PASSWORD")
-MONGODB_DB = os.environ.get("MONGODB_DB")
+MONGODB_HOST = config('MONGODB_HOST', default=os.environ.get("MONGODB_HOST"))
+MONGODB_PORT = config('MONGODB_PORT', default=os.environ.get("MONGODB_PORT"))
+MONGODB_USERNAME = config('MONGODB_USERNAME',
+                          default=os.environ.get("MONGODB_USERNAME"))
+MONGODB_PASSWORD = config('MONGODB_PASSWORD',
+                          default=os.environ.get("MONGODB_PASSWORD"))
+MONGODB_DB = config('MONGODB_DB', default=os.environ.get("MONGODB_DB"))
 
 # Create the application instance
 connexionapp = connexion.App(__name__, specification_dir='./')
@@ -33,7 +41,8 @@ app.secret_key = '\\\xfcS\x1e\x8f\xfb]6\x1e.\xa8\xb3\xe1x\xc8\x8e\xc1\xeb5^x\x81
 csrf = CSRFProtect(app)
 SESSION_TYPE = 'filesystem'
 app.config.from_object(__name__)
-app.config['passkey'] = os.environ.get("KEY")
+app.config['passkey'] = config('KEY', default=os.environ.get("KEY"))
+
 app.config['GOOGLE_CLIENT_ID'] = GOOGLE_CLIENT_ID
 app.config['GOOGLE_CLIENT_SECRET'] = GOOGLE_CLIENT_SECRET
 app.config['REDIRECT_URI'] = REDIRECT_URI
