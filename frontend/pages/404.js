af2b9700a77cd204b7b1b6a5499d6cbbfeb8d795
Sushant Bansal <sushantbansal2@gmail.com>
2020-07-08 18:02:37 -0500

    Added home page and 404 page

---
+++ frontend/pages/404.js
@@ -0,0 +1,42 @@
+import Layout from "../components/layout";
+import Link from "next/link";
+import StyledButton from "../components/button";
+import { Box, Typography, Container } from "@material-ui/core";
+
+export default () => {
+  return (
+    <Layout>
+      <Box
+        display="flex"
+        flexGrow={1}
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Container>
+          <Typography variant="h2" align="center" gutterBottom>
+            <Box fontWeight="bold">
+              {" "}
+              Oops! <br /> The page you're looking for does not exist.
+            </Box>
+          </Typography>
+          <Typography variant="h6" align="center">
+            If you think this is an error, please{" "}
+            <Link href="/contact">
+              <a>contact</a>
+            </Link>{" "}
+            us!
+          </Typography>
+          <Box display="flex" flexDirection="row" m={4} justifyContent="center">
+            <StyledButton text="Go to Explorer" url="/explorer"></StyledButton>
+            <StyledButton text="Go to Curator" url="/curator"></StyledButton>
+          </Box>
+        </Container>
+      </Box>
+      <style jsx>{`
+        a {
+          color: #9a0000;
+        }
+      `}</style>
+    </Layout>
+  );
+};
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,10 +1,67 @@
-import Head from 'next/head'
-import Layout from "../components/layout"
+import Layout from "../components/layout";
+import StyledButton from "../components/button";
+import SEO from "../components/seo";
+import { Box, Typography, Container } from "@material-ui/core";
 
 export default function Home() {
   return (
     <Layout>
-      Body
+      {/* <SEO title="Qresp" description="" author=""></SEO> */}
+      <Box display="flex" flexDirection="column" flexGrow={1}>
+        <Box className="full-width">
+          <img
+            src="/images/qresp-poster.png"
+            height="478px"
+            width="100%"
+            className="blur"
+          ></img>
+          <img
+            src="/images/qresp-poster.png"
+            height="478px"
+            width="60%"
+            className="poster"
+          ></img>
+        </Box>
+        <Box display="flex" m={3} alignItems="center" justifyContent="center">
+          <Container>
+            <Typography variant="h5" align="center" gutterBottom>
+              <Box fontWeight="fontWeightBold">
+                The open source software Qresp "Curation and Exploration of
+                Reproducible Scientific Papers" <br /> facilitates the
+                organization, annotation and exploration of data presented in
+                scientific papers.
+              </Box>
+            </Typography>
+            <Box
+              display="flex"
+              flexDirection="row"
+              justifyContent="center"
+              p={1}
+            >
+              <StyledButton text="Explorer" url="/explorer" />
+              <StyledButton text="Curator" url="/curator" />
+            </Box>
+          </Container>
+        </Box>
+      </Box>
+      <style jsx>
+        {`
+          .full-width {
+            width: 100%;
+          }
+          .poster {
+            object-fit: cover;
+            object-position: left top;
+            position: absolute;
+            left: 50%;
+            transform: translate(-50%);
+          }
+
+          .blur {
+            filter: blur(25px);
+          }
+        `}
+      </style>
     </Layout>
-  )
+  );
 }
