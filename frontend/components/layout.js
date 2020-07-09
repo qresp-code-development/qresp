94d2363c2f8620d1ca42b5085419cb081c104e1f
Sushant Bansal <sushantbansal2@gmail.com>
2020-07-09 18:28:41 -0500

    Added Proptypes

--- frontend/components/layout.js
+++ frontend/components/layout.js
@@ -1,18 +1,22 @@
-import Header from "../components/Header/header"
-import Footer from "../components/Footer/footer"
-import { Box } from "@material-ui/core"
+import Header from "../components/Header/header";
+import Footer from "../components/Footer/footer";
+import { Box } from "@material-ui/core";
+import PropTypes from "prop-types";
 
 function Layout({ children }) {
-
-    return (
-        <Box display="flex" flexDirection="column" flexGrow={1}>
-            <Header />
-            <Box display="flex" flexGrow={1}>
-                {children}
-            </Box>
-            <Footer />
-        </Box>
-    )
+  return (
+    <Box display="flex" flexDirection="column" flexGrow={1}>
+      <Header />
+      <Box display="flex" flexGrow={1}>
+        {children}
+      </Box>
+      <Footer />
+    </Box>
+  );
 }
 
-export default Layout
+Layout.propTypes = {
+  children: PropTypes.object.isRequired,
+};
+
+export default Layout;
