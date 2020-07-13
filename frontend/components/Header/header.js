53f45cddf3a5b1f257eb9c2e91793fea1ee31ca7
Sushant Bansal <sushantbansal2@gmail.com>
2020-07-13 18:50:48 -0500

    Adding responsiveness

--- frontend/components/Header/header.js
+++ frontend/components/Header/header.js
@@ -1,8 +1,55 @@
-import { AppBar, Toolbar, Box, Container, Button } from "@material-ui/core";
-import { InternalStyledButton, ExternalStyledButton } from "../button";
+import { useState, Fragment } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Box,
+  Container,
+  Button,
+  Hidden,
+  SwipeableDrawer,
+} from "@material-ui/core";
+
+import { Menu } from "@material-ui/icons";
+
+import StyledButton, {
+  InternalStyledButton,
+  ExternalStyledButton,
+} from "../button";
 import Link from "next/link";
 
 const Header = () => {
+  const [drawer, setDrawer] = useState(false);
+
+  const handleOpen = () => {
+    setDrawer(true);
+  };
+
+  const toggleDrawer = () => {
+    if (
+      event &&
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+    setDrawer(!drawer);
+  };
+
+  const links = (
+    <Fragment>
+      {" "}
+      <InternalStyledButton text="Explorer" url="/explorer" />
+      <InternalStyledButton text="Curator" url="/curator" />
+      <ExternalStyledButton
+        text="Documentation"
+        url="https://qresp.org"
+        external={true}
+      />
+      <InternalStyledButton text="Contact" url="/contact" />
+      <InternalStyledButton text="LogIn" url="/login" />
+    </Fragment>
+  );
+
   return (
     <AppBar position="sticky" color="primary" elevation={0}>
       <Toolbar>
@@ -10,9 +57,9 @@ const Header = () => {
           <Box
             display="flex"
             flexDirection="row"
-            m={2}
             flexGrow={1}
             alignItems="center"
+            m={2}
           >
             <Box display="flex" alignItems="center" flexGrow={1} m={1}>
               <Link href="/">
@@ -25,18 +72,25 @@ const Header = () => {
                 </Button>
               </Link>
             </Box>
-            <InternalStyledButton text="Explorer" url="/explorer" />
-            <InternalStyledButton text="Curator" url="/curator" />
-            <ExternalStyledButton
-              text="Documentation"
-              url="https://qresp.org"
-              external={true}
-            />
-            <InternalStyledButton text="Contact" url="/contact" />
-            <InternalStyledButton text="LogIn" url="/login" />
+            <Box display="flex">
+              <Hidden smDown>{links}</Hidden>
+              <Hidden mdUp>
+                <StyledButton onClick={handleOpen}>
+                  <Menu />
+                </StyledButton>
+              </Hidden>
+            </Box>
           </Box>
         </Container>
       </Toolbar>
+      <SwipeableDrawer
+        anchor="top"
+        open={drawer}
+        onClose={toggleDrawer}
+        onOpen={toggleDrawer}
+      >
+        A
+      </SwipeableDrawer>
     </AppBar>
   );
 };
