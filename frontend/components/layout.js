3f33399c9b823508634060b2f38c46d042c087bb
Sushant Bansal <sushantbansal2@gmail.com>
2020-07-13 16:10:16 -0500

    Added Alert Component to Layout

--- frontend/components/layout.js
+++ frontend/components/layout.js
@@ -2,11 +2,13 @@ import Header from "../components/Header/header";
 import Footer from "../components/Footer/footer";
 import { Box } from "@material-ui/core";
 import PropTypes from "prop-types";
+import AlertDialog from "./alert";
 
 function Layout({ children }) {
   return (
     <Box display="flex" flexDirection="column" flexGrow={1}>
       <Header />
+      <AlertDialog />
       <Box display="flex" flexGrow={1}>
         {children}
       </Box>
