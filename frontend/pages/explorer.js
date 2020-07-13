43f64f27e8eba7f3a81871deaf0df5d76652dee4
Sushant Bansal <sushantbansal2@gmail.com>
2020-07-13 18:51:06 -0500

    Finishing touches left

--- frontend/pages/explorer.js
+++ frontend/pages/explorer.js
@@ -1,19 +1,74 @@
-import { Fragment } from "react";
-import StyledButton from "../components/button";
-import SEO from "../components/seo";
-import { Box, Typography, Container, TextField } from "@material-ui/core";
-import axios from "axios";
+import { Fragment, useState, useContext, useEffect } from "react";
+import Router from "next/router";
 
+import StyledButton, { SmallStyledButton } from "../components/button";
+import SEO from "../components/seo";
+
+import apiEndpoint from "../Context/axios";
+
+import { Box, Typography, Container, TextField } from "@material-ui/core";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 
-const explorer = ({ servers }) => {
+import AlertContext from "../Context/Alert/alertContext";
+
+const explorer = ({ servers, error }) => {
+  const { setAlert, unsetAlert } = useContext(AlertContext);
+
   const explorerDescription =
     "The explorer provides a portal for the scientific community to access datasets, explore workflows and download curated data, published in scientific papers.";
   const explorerAuthor = "Giulia Galli, Macro Govoni";
 
+  const [selectedServers, setSelectedServers] = useState("");
+
+  // Get the list of selected servers, on change in list
   const handleChange = (event, values) => {
-    console.log(values);
+    setSelectedServers(values.join(","));
   };
+
+  const refresh = () => {
+    Router.reload();
+  };
+
+  const searchSelected = () => {
+    if (selectedServers.length === 0) {
+      const title = "Error, No nodes selected";
+      const msg =
+        "You didn't select any servers. Did you mean to search on all of them ?";
+      const buttons = (
+        <SmallStyledButton onClick={searchAll}>Search All</SmallStyledButton>
+      );
+      setAlert(title, msg, buttons);
+      return;
+    }
+    alert("Nice", selectedServers);
+  };
+
+  const searchAll = () => {
+    unsetAlert();
+    alert("Searching on All Servers", servers);
+  };
+
+  const errortitle = "Oops!";
+  const errormsg = (
+    <Fragment>
+      There was an error trying to get the available Qresp nodes! <br />
+      If problems persist please contact the administrator
+    </Fragment>
+  );
+
+  if (error) {
+    servers = [];
+  }
+
+  useEffect(() => {
+    if (error) {
+      setAlert(
+        errortitle,
+        errormsg,
+        <SmallStyledButton onClick={refresh}>Retry</SmallStyledButton>
+      );
+    }
+  }, []);
 
   return (
     <Fragment>
@@ -29,31 +84,29 @@ const explorer = ({ servers }) => {
               <Box fontWeight="bold">Select Qresp node to search</Box>
             </Typography>
           </Box>
-          <Box
-            display="flex"
-            flexDirection="row"
-            alignItems="center"
-            justifyContent="center"
-            flexGrow={1}
-            m={4}
-          >
-            <Autocomplete
-              multiple
-              options={servers}
-              getOptionLabel={(option) => option.qresp_server_url}
-              filterSelectedOptions
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  variant="outlined"
-                  label="Select one or more nodes!"
-                />
-              )}
-              fullWidth
-              ChipProps={{ color: "primary", variant: "outlined" }}
-              onChange={handleChange}
-            />
-            <StyledButton text="Search" url="" external={true} />
+          <Autocomplete
+            multiple
+            options={servers}
+            getOptionLabel={(option) => option.qresp_server_url}
+            filterSelectedOptions
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                variant="outlined"
+                label="Select one or more nodes!"
+              />
+            )}
+            fullWidth
+            ChipProps={{ color: "primary", variant: "outlined" }}
+            onChange={handleChange}
+          />
+          <Box display="flex" flexDirection="row" justifyContent="center" m={4}>
+            <StyledButton onClick={searchSelected} disabled={error}>
+              Search Selected
+            </StyledButton>
+            <StyledButton onClick={searchAll} disabled={error}>
+              Search All
+            </StyledButton>
           </Box>
           <Box display="flex" alignItems="center" justifyContent="center" m={4}>
             <Typography variant="h5" align="center">
@@ -73,7 +126,7 @@ const explorer = ({ servers }) => {
             border-radius: 5px;
             border-color: rgba(0, 0, 0, 0.125);
             margin: 40px;
-            padding: 8px;
+            padding: 40px;
           }
         `}
       </style>
@@ -81,25 +134,20 @@ const explorer = ({ servers }) => {
   );
 };
 
-export const getStaticProps = async () => {
-  const servers = [
-    {
-      qresp_server_url: "https://paperstack.uchicago.edu",
-      isActive: "Yes",
-      qresp_maintainer_emails: ["datadev@lists.uchicago.edu"],
-    },
-  ];
+export async function getServerSideProps(ctx) {
+  var error = false;
+  var servers = [];
+
   try {
-    const servers = await axios.get("http://localhost:5000/qrespservers");
-  } catch (error) {
-    console.error(error);
+    const response = await apiEndpoint.get("/static/qresp_servers.json");
+    servers = response.data;
+  } catch (e) {
+    error = true;
   }
 
   return {
-    props: {
-      servers,
-    },
+    props: { servers, error },
   };
-};
+}
 
 export default explorer;
