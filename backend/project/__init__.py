c004207a3373b63ab475fd3abfa0af7bb4fbf3bc
Sushant Bansal <sushantbansal2@gmail.com>
2020-07-20 18:41:36 -0500

    Added app level environment variable

--- backend/project/__init__.py
+++ backend/project/__init__.py
@@ -20,19 +20,21 @@ app = connexionapp.app
 app.secret_key = Config.get_setting('SECRETS','FLASK_SECRET_KEY')
 SESSION_TYPE = 'filesystem'
 app.config.from_object(__name__)
+app.config['env'] = 'DEV'
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 Session(app)
 ext = Sitemap(app)
 CORS(app)
 
+
 #initialize db
-if Config.get_setting('PROD','MONGODB_HOST'):
+if Config.get_setting(app.config['env'],'MONGODB_HOST'):
     db = MongoEngine()
-    app.config['MONGODB_HOST'] = Config.get_setting('PROD','MONGODB_HOST')
-    app.config['MONGODB_PORT'] = int(Config.get_setting('PROD','MONGODB_PORT'))
-    app.config['MONGODB_USERNAME'] = Config.get_setting('PROD','MONGODB_USERNAME')
-    app.config['MONGODB_PASSWORD'] = Config.get_setting('PROD','MONGODB_PASSWORD')
-    app.config['MONGODB_DB'] = Config.get_setting('PROD','MONGODB_DB_NAME')
+    app.config['MONGODB_HOST'] = Config.get_setting(app.config['env'],'MONGODB_HOST')
+    app.config['MONGODB_PORT'] = int(Config.get_setting(app.config['env'],'MONGODB_PORT'))
+    app.config['MONGODB_USERNAME'] = Config.get_setting(app.config['env'],'MONGODB_USERNAME')
+    app.config['MONGODB_PASSWORD'] = Config.get_setting(app.config['env'],'MONGODB_PASSWORD')
+    app.config['MONGODB_DB'] = Config.get_setting(app.config['env'],'MONGODB_DB_NAME')
     db.init_app(app)
 
 from project import routes
