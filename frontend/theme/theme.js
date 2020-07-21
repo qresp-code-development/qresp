0de1d16e1979f3ae19b9eb00a735df027f040d35
Sushant Bansal <sushantbansal2@gmail.com>
2020-07-21 18:46:39 -0500

    Changed secondary color

--- frontend/theme/theme.js
+++ frontend/theme/theme.js
@@ -1,14 +1,14 @@
-import { createMuiTheme } from '@material-ui/core/styles'
+import { createMuiTheme } from "@material-ui/core/styles";
 
 const Theme = createMuiTheme({
-    palette: {
-        primary: {
-            main: '#800000'
-        },
-        secondary: {
-            main: '#FFF'
-        },
-    }
+  palette: {
+    primary: {
+      main: "#800000",
+    },
+    secondary: {
+      main: "#777777",
+    },
+  },
 });
 
-export default Theme
+export default Theme;
