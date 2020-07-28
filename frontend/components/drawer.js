a975df86fc7715bfc5e751896306331cc581e884
Sushant Bansal <sushantbansal2@gmail.com>
2020-07-28 18:15:51 -0500

    Separated out Accordion Drawer and Tooltip for reuse

---
+++ frontend/components/drawer.js
@@ -0,0 +1,42 @@
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  withStyles,
+  Typography,
+  Box,
+} from "@material-ui/core";
+import { ExpandMore } from "@material-ui/icons";
+
+const StyledAccordion = withStyles({
+  root: {
+    borderRadius: "5px",
+  },
+})(Accordion);
+
+const StyledAccordionSummary = withStyles({
+  root: {
+    backgroundColor: "rgba(0,0,0,.03)",
+  },
+})(AccordionSummary);
+
+const Drawer = (props) => {
+  const { heading, children } = props;
+  return (
+    <StyledAccordion elevation={4} square={true}>
+      <StyledAccordionSummary expandIcon={<ExpandMore />}>
+        <Typography variant="h4">
+          <Box fontWeight="bold">{heading}</Box>
+        </Typography>
+      </StyledAccordionSummary>
+      <AccordionDetails>
+        <Box display="flex" flexDirection="column" style={{ width: "100%" }}>
+          {children}
+        </Box>
+      </AccordionDetails>
+    </StyledAccordion>
+  );
+};
+
+export { StyledAccordion, StyledAccordionSummary };
+export default Drawer;
