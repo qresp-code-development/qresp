a9fa72a57f6fa7adc37667b5a054b70c15108acf
Sushant Bansal <sushantbansal2@gmail.com>
2020-08-10 17:52:50 -0500

    Added frontend request handler for chart workflow

---
+++ frontend/pages/api/chartworkflow.js
@@ -0,0 +1,15 @@
+import apiEndpoint from "../../Context/axios";
+
+export default async (req, res) => {
+  if (req.method == "POST") {
+    try {
+      const response = await apiEndpoint.get(
+        `/api/paper/${req.body.paperid}/chart/${req.body.chartid}`
+      );
+      res.status(200).json(response.data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json(error);
+    }
+  }
+};
