5277a12eba75f04d3ad63eaa493b7c1549e9e2be
Sushant Bansal <sushantbansal2@gmail.com>
2020-08-10 17:56:31 -0500

    Using utilies instead of defining functions in here

--- frontend/components/Paper/Workflow.js
+++ frontend/components/Paper/Workflow.js
@@ -5,6 +5,7 @@ import useMediaQuery from "@material-ui/core/useMediaQuery";
 import Drawer from "../drawer";
 import Graph from "../Workflow/Graph";
 import Legend from "../Workflow/Legend";
+import { formatData } from "../Workflow/util";
 
 import { Box, Grid, useTheme } from "@material-ui/core";
 
@@ -14,29 +15,7 @@ const Workflow = ({ workflow, charts, tools, scripts, datasets, external }) => {
     ? "row"
     : "column";
 
-  const data = {
-    c: {},
-    t: {},
-    s: {},
-    d: {},
-    h: {},
-  };
-
-  charts.forEach((element) => {
-    data.c[element.id] = element;
-  });
-  tools.forEach((element) => {
-    data.t[element.id] = element;
-  });
-  scripts.forEach((element) => {
-    data.s[element.id] = element;
-  });
-  datasets.forEach((element) => {
-    data.d[element.id] = element;
-  });
-  external.forEach((element) => {
-    data.h[element.id] = element;
-  });
+  const data = formatData(charts, tools, external, datasets, scripts);
 
   return (
     <Drawer heading="Workflow">
