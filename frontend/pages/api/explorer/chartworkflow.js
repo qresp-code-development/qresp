cf82b6a394e730c0911aaa49435b021b08cfbc64
Sushant Bansal <sushantbansal2@gmail.com>
2020-08-17 18:31:06 -0500

    Fixed endpoint, updated required parameters

--- frontend/pages/api/explorer/chartworkflow.js
+++ frontend/pages/api/explorer/chartworkflow.js
@@ -3,9 +3,13 @@ import apiEndpoint from "../../../Context/axios";
 export default async (req, res) => {
   if (req.method == "POST") {
     try {
-      const response = await apiEndpoint.get(
-        `/api/paper/${req.body.paperid}/chart/${req.body.chartid}`
-      );
+      const response = await apiEndpoint.get(`/chartworkflow`, {
+        params: {
+          chartid: req.body.chartid,
+          paperid: req.body.paperid,
+          servers: req.body.servers,
+        },
+      });
       res.status(200).json(response.data);
     } catch (error) {
       console.error(error);
