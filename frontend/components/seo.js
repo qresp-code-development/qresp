159f634b48f58fa725b2e5c8165435d0a3edb120
Sushant Bansal <sushantbansal2@gmail.com>
2020-08-26 14:27:15 -0500

    Removed redundant open graph tags

--- frontend/components/seo.js
+++ frontend/components/seo.js
@@ -6,13 +6,15 @@ const SEO = (props) => {
   return (
     <Head>
       <title>{title}</title>
-      <meta property="description" content={description} />
-      <meta property="og:title" content="My page title" key="title" />
+      <meta property="og:title" content={title} key="title" />
       <meta property="og:description" content={description} key="description" />
+      <meta
+        property="og:image"
+        content="https://github.com/anti-mony/qresp/raw/ReWrite/frontend/public/images/QrespLogoDark.png"
+      />
       <meta property="og:type" content="website" key="type" />
-      <meta property="twitter:card" content="summary" key="twittersummary" />
       <meta property="twitter:creator" content={author} key="twitterauthor" />
-      <meta property="twitter:description" content={title} key="twittertitle" />
+
       <meta
         property="twitter:creator"
         content={description}
