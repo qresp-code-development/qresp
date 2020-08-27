8771a918ba0fa3ba9ad0f2a44f8b4264b803a693
Sushant Bansal <sushantbansal2@gmail.com>
2020-08-27 18:25:22 -0500

    Removed duplicate open graph tags

--- frontend/components/seo.js
+++ frontend/components/seo.js
@@ -13,13 +13,9 @@ const SEO = (props) => {
         content="https://github.com/anti-mony/qresp/raw/ReWrite/frontend/public/images/QrespLogoDark.png"
       />
       <meta property="og:type" content="website" key="type" />
-      <meta property="twitter:creator" content={author} key="twitterauthor" />
-
-      <meta
-        property="twitter:creator"
-        content={description}
-        key="twitterdescription"
-      />
+      {author ? (
+        <meta property="twitter:creator" content={author} key="twitterauthor" />
+      ) : null}
     </Head>
   );
 };
@@ -27,7 +23,6 @@ const SEO = (props) => {
 SEO.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
 };
 
 export default SEO;
