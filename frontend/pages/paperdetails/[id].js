5a219b412bf74426dd3a8a7e90f1a9959a248547
Sushant Bansal <sushantbansal2@gmail.com>
2020-08-27 18:26:54 -0500

    Removed unused imports

--- frontend/pages/paperdetails/[id].js
+++ frontend/pages/paperdetails/[id].js
@@ -1,4 +1,4 @@
-import { Fragment, useContext, useEffect, useState } from "react";
+import { Fragment, useContext, useEffect } from "react";
 import PropTypes from "prop-types";
 
 import { useRouter } from "next/router";
