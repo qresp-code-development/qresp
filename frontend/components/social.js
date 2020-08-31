f9c348e42459b39a8e1f328964d6c13765629946
Sushant Bansal <sushantbansal2@gmail.com>
2020-08-31 17:47:31 -0500

    Added social share for email, fb, twitter and link copy

--- frontend/components/social.js
+++ frontend/components/social.js
@@ -1,11 +1,17 @@
 import { Fragment, useState } from "react";
 import PropTypes from "prop-types";
 
-import { Popover, IconButton, Paper } from "@material-ui/core";
-import { Share, Facebook, Twitter, Link } from "@material-ui/icons";
+import { Popover, IconButton, Paper, Snackbar } from "@material-ui/core";
+import { Share, Facebook, Twitter, Link, Email } from "@material-ui/icons";
+import { Alert } from "@material-ui/lab";
 
 const SocialShare = ({ url }) => {
   const [anchorEl, setAnchorEl] = useState(null);
+  const [alert, setAlert] = useState({
+    show: false,
+    type: "success",
+    msg: "Link copied successfully",
+  });
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -15,17 +21,48 @@ const SocialShare = ({ url }) => {
     setAnchorEl(null);
   };
 
+  const handleAlertClose = (event, reason) => {
+    setAlert({ ...alert, show: false });
+  };
+
+  const shareFacebook = () => {
+    const link = window.location.href;
+    window.open(
+      `https://www.facebook.com/sharer/sharer.php?u=${link}`,
+      "_blank"
+    );
+    handleClose();
+  };
+
+  const shareTwitter = () => {
+    const link = window.location.href;
+    window.open(`https://twitter.com/home?status=${link}`, "_blank");
+    handleClose();
+  };
+
+  const shareEmail = () => {
+    const link = window.location.href;
+    window.location.href = `mailto:?subject=${document.title}body=Please have a look at:%0D%0A${link}`;
+  };
+
   const shareLink = () => {
     const link = window.location.href;
+    console.log(navigator);
     navigator.clipboard
       .writeText(link)
       .then(() => {
-        console.log("Text copied to clipboard");
+        setAlert({ ...alert, show: true });
       })
       .catch((err) => {
         // This can happen if the user denies clipboard permissions:
-        console.error("Could not copy text: ", err);
+        setAlert({
+          ...alert,
+          show: true,
+          type: "error",
+          msg: "There was an error copying the url",
+        });
       });
+
     handleClose();
   };
 
@@ -50,17 +87,33 @@ const SocialShare = ({ url }) => {
         }}
       >
         <Paper>
-          <IconButton>
-            <Facebook />
+          <IconButton onClick={shareFacebook}>
+            <Facebook style={{ color: "#1877f2" }} />
           </IconButton>
-          <IconButton>
-            <Twitter />
+          <IconButton onClick={shareTwitter}>
+            <Twitter style={{ color: "#1da0f2" }} />
+          </IconButton>
+          <IconButton onClick={shareEmail}>
+            <Email />
           </IconButton>
           <IconButton onClick={shareLink}>
-            <Link />
+            <Link color="primary" />
           </IconButton>
         </Paper>
       </Popover>
+      <Snackbar
+        open={alert.show}
+        autoHideDuration={5000}
+        onClose={handleAlertClose}
+      >
+        <Alert
+          severity={alert.type}
+          variant="filled"
+          onClose={handleAlertClose}
+        >
+          {alert.msg}
+        </Alert>
+      </Snackbar>
     </Fragment>
   );
 };
