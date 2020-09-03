271eaffd4901185a9f1062b9344c7e123cdf212d
Sushant Bansal <sushantbansal2@gmail.com>
2020-09-03 18:25:55 -0500

    Added rescer to set curator info

--- frontend/Context/Curator/curatorReducer.js
+++ frontend/Context/Curator/curatorReducer.js
@@ -1,5 +1,12 @@
+import { SET_CURATORINFO } from "../types";
+
 export default (state, action) => {
-  switch (action) {
+  switch (action.type) {
+    case SET_CURATORINFO:
+      return {
+        ...state,
+        curatorInfo: action.payload,
+      };
     default:
       return state;
   }
