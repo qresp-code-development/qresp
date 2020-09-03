0477f9beafa74d54a120b298f4ed60a46230f802
Sushant Bansal <sushantbansal2@gmail.com>
2020-09-02 19:00:16 -0500

    Added helper text tooltip, props to control the component

--- frontend/components/Form/TextInput.js
+++ frontend/components/Form/TextInput.js
@@ -1,66 +1,65 @@
 import { Fragment, useState } from "react";
 import PropTypes from "prop-types";
 
-import { TextField, Typography } from "@material-ui/core";
+import { TextField, Typography, Tooltip } from "@material-ui/core";
 
-import { Formik, useField } from "formik";
+import { useField } from "formik";
 
 const TextInput = (props) => {
-  const { label, id, placeholder, type, helperText, name, required } = props;
+  const { id, placeholder, type, helperText, name } = props;
 
   const [field, meta] = useField(props);
   const [focused, setFocused] = useState(false);
+  const [hovering, setHovering] = useState(false);
 
   return (
-    <Fragment>
-      {label ? (
-        <Typography variant="h6" color="secondary">
-          {label}
-          {required ? <span style={{ color: "red" }}> *</span> : null}
-        </Typography>
-      ) : null}
+    <Tooltip
+      title={
+        helperText ? (
+          <Typography variant="subtitle2">{helperText}</Typography>
+        ) : (
+          ""
+        )
+      }
+      placement="top"
+      arrow
+      open={focused || hovering}
+    >
       <TextField
         name={name}
-        id={id}
         fullWidth
         variant="outlined"
         placeholder={placeholder}
         type={type}
         {...field}
         error={meta.touched && meta.error && !focused}
-        helperText={
-          meta.touched && meta.error && !focused
-            ? meta.error
-            : focused
-            ? helperText
-            : ""
-        }
-        onFocus={() => {
-          setFocused(true);
-        }}
-        onBlur={(e) => {
-          field.onBlur(e);
-          setFocused(false);
+        helperText={meta.touched && meta.error && !focused ? meta.error : ""}
+        InputProps={{
+          onFocus: () => setFocused(true),
+          onBlur: (e) => {
+            field.onBlur(e);
+            setFocused(false);
+          },
+          onMouseEnter: () => setHovering(true),
+          onMouseLeave: () => setHovering(false),
+          id: id,
         }}
       />
-    </Fragment>
+    </Tooltip>
   );
 };
 
 TextInput.defaultProps = {
   type: "text",
   helperText: "",
-  required: false,
 };
 
 TextInput.propTypes = {
   id: PropTypes.string.isRequired,
-  label: PropTypes.string,
   placeholder: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   type: PropTypes.string,
   helperText: PropTypes.string,
-  required: PropTypes.bool,
 };
 
 export default TextInput;
