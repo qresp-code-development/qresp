220aa1a0ea2b80a9ce6ce68d5d08acc5802afd7d
Sushant Bansal <sushantbansal2@gmail.com>
2020-09-04 18:29:08 -0500

    Curator Info in Progress

--- frontend/components/Curator/LocationInfo.js
+++ frontend/components/Curator/LocationInfo.js
@@ -1,7 +1,43 @@
 import Drawer from "../drawer";
+import RadioInput from "../Form/RadioInput";
+
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
 
 const LocationInfo = () => {
-  return <Drawer heading="Where is the paper"></Drawer>;
+  const options = [
+    {
+      label: "HTTP Connection",
+      value: "http",
+    },
+    {
+      label: "Zenodo",
+      value: "zenodo",
+    },
+  ];
+  return (
+    <Drawer heading="Where is the paper">
+      <Formik
+        initialValues={{ connectiontype: "" }}
+        validationSchema={Yup.object({
+          connectiontype: Yup.string().required("Required"),
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          alert(JSON.stringify(values));
+          setSubmitting(false);
+        }}
+      >
+        <Form>
+          <RadioInput
+            name="connectiontype"
+            helperText="Select connection type"
+            options={options}
+          />
+          <button type="submit">TEST BUTTON</button>
+        </Form>
+      </Formik>
+    </Drawer>
+  );
 };
 
 export default LocationInfo;
