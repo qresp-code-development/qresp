0ed688d29e4c5d60a98fb0aeba724687e8e52f63
Sushant Bansal <sushantbansal2@gmail.com>
2020-09-14 18:23:53 -0500

    Removed unused imports

--- frontend/Context/Curator/CuratorState.js
+++ frontend/Context/Curator/CuratorState.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useEffect } from "react";
+import { useReducer, useEffect } from "react";
 import CuratorReducer from "./curatorReducer";
 import CuratorContext from "./curatorContext";
 
