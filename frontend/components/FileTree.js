fe303ddd55bcda8b697dbe663a5f5160b1523df1
Sushant Bansal <sushantbansal2@gmail.com>
2020-09-14 18:22:23 -0500

    Dialog for file tree selector

--- frontend/components/FileTree.js
+++ frontend/components/FileTree.js
@@ -1,7 +1,70 @@
+import { useContext } from "react";
+
 import TreeSelect, { TreeNode, SHOW_PARENT } from "rc-tree-select";
 
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+} from "@material-ui/core";
+
+import { RegularStyledButton } from "../components/button";
+
+import SourceTreeContext from "../Context/SourceTree/SourceTreeContext";
+
 const FileTree = () => {
-  return <div></div>;
+  const {
+    tree,
+    showSelector,
+    closeSelector,
+    selected,
+    selectedMultiple,
+    multiple,
+    title,
+    setSelected,
+    setSelectedMultiple,
+  } = useContext(SourceTreeContext);
+
+  const onChange = (value, ...rest) => {
+    console.log("onSingleCheck", value, rest);
+    setSelected(value);
+  };
+
+  const onMultipleChange = (value) => {
+    console.log("onMultipleChange", value);
+    setSelectedMultiple(value);
+  };
+
+  return (
+    <Dialog open={showSelector} onClose={closeSelector}>
+      <DialogTitle onClose={closeSelector}>{title}</DialogTitle>
+      <DialogContent>
+        <TreeSelect
+          style={{ zIndex: 100000 }}
+          transitionName="rc-tree-select-dropdown-slide-up"
+          choiceTransitionName="rc-tree-select-selection__choice-zoom"
+          dropdownStyle={{
+            maxHeight: "50vh",
+            overflow: "auto",
+            zIndex: 100000,
+          }}
+          multiple={multiple}
+          value={multiple ? selectedMultiple : selected}
+          treeData={tree}
+          treeNodeFilterProp="title"
+          onChange={multiple ? onMultipleChange : onChange}
+          allowClear
+        />
+      </DialogContent>
+      <DialogActions>
+        <RegularStyledButton>Save</RegularStyledButton>
+        <RegularStyledButton onClick={closeSelector}>
+          Cancel
+        </RegularStyledButton>
+      </DialogActions>
+    </Dialog>
+  );
 };
 
 export default FileTree;
