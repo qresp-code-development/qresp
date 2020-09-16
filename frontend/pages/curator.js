7de2cee81b0a0b20bb4023ff20ba3dcafae635ae
Sushant Bansal <sushantbansal2@gmail.com>
2020-09-16 18:01:31 -0500

    Removed old components, added new element components

--- frontend/pages/curator.js
+++ frontend/pages/curator.js
@@ -5,9 +5,9 @@ import CuratorState from "../Context/Curator/CuratorState";
 import SourceTreeState from "../Context/SourceTree/SourceTreeState";
 
 import SEO from "../components/seo";
-import TopActions from "../components/Curator/TopActions";
-import CuratorInfo from "../components/Curator/CuratorInfo";
-import LocationInfo from "../components/Curator/LocationInfo";
+import TopActions from "../components/CuratorElements/TopActions";
+import CuratorElement from "../components/CuratorElements/CuratorElement";
+import LocationForm from "../components/CuratorForms/LocationInfoForm";
 import FileTree from "../components/FileTree";
 
 const curator = () => {
@@ -23,8 +23,7 @@ const curator = () => {
           <Box mt={4} mb={4}>
             <TopActions />
           </Box>
-          <CuratorInfo />
-          <LocationInfo />
+          <CuratorElement />
         </Container>
       </SourceTreeState>
     </CuratorState>
