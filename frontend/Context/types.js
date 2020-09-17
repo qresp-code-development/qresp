9986bc8cee1aa11eabb45902048aedffb90dfac0
Sushant Bansal <sushantbansal2@gmail.com>
2020-09-17 18:19:17 -0500

    Added new types

--- frontend/Context/types.js
+++ frontend/Context/types.js
@@ -14,12 +14,14 @@ export const HIDE_LOADER = "HIDE_LOADER";
 export const SET_SELECTED = "SET_SELECTED";
 export const SET_SELECTED_HTTP = "SET_SELECTED_HTTP";
 
-// Curator Actions
-export const SET_CURATORINFO = "SET_CURATORINFO";
-
 // Source Tree
 export const SET_TREE = "SET_TREE";
 export const SHOW_TREE_SELECTOR = "SHOW_TREE_SELECTOR";
 export const HIDE_TREE_SELECTOR = "HIDE_TREE_SELECTOR";
 export const SET_FILETREE_CHECKED = "SET_CHECKED";
 export const TOGGLE_MULTIPLE = "TOGGLE_MULTIPLE";
+
+// Curator Actions
+export const SET_ALL = "SET_ALL";
+export const SET_CURATORINFO = "SET_CURATORINFO";
+export const SET_FILESERVERPATH = "SET_FILESERVERPATH";
