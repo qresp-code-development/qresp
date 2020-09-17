e6891ac9702964c111e72f55bd64cd4c7f0864a5
Sushant Bansal <sushantbansal2@gmail.com>
2020-09-17 18:22:30 -0500

    Added support to store data on browser, more functions

--- frontend/Context/Curator/CuratorState.js
+++ frontend/Context/Curator/CuratorState.js
@@ -2,7 +2,9 @@ import { useReducer, useEffect } from "react";
 import CuratorReducer from "./curatorReducer";
 import CuratorContext from "./curatorContext";
 
-import { SET_CURATORINFO } from "../types";
+import WebStore from "../../Utils/Persist";
+
+import { SET_ALL, SET_CURATORINFO, SET_FILESERVERPATH } from "../types";
 
 const CuratorState = (props) => {
   const initialState = {
@@ -16,12 +18,31 @@ const CuratorState = (props) => {
     referenceInfo: {
       authors: [{ firstName: "", middleName: "", lastName: "" }],
     },
+    fileServerPath: "",
   };
 
   const [state, dispatch] = useReducer(CuratorReducer, initialState);
 
+  useEffect(() => {
+    const data = WebStore.get("state");
+    if (data !== null) {
+      setAll(data);
+    }
+  }, []);
+
+  useEffect(() => {
+    WebStore.set("state", state);
+  }, [state]);
+
+  const setAll = (data) => {
+    dispatch({ type: SET_ALL, payload: data });
+  };
+
   const setCuratorInfo = (info) => {
     dispatch({ type: SET_CURATORINFO, payload: info });
+  };
+  const setFileServerPath = (path) => {
+    dispatch({ type: SET_FILESERVERPATH, payload: path });
   };
 
   return (
@@ -29,8 +50,10 @@ const CuratorState = (props) => {
       value={{
         reference: state.reference,
         curatorInfo: state.curatorInfo,
+        fileServerPath: state.fileServerPath,
         metadata: state,
         setCuratorInfo: setCuratorInfo,
+        setFileServerPath: setFileServerPath,
       }}
     >
       {props.children}
