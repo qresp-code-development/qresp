b1c719c94fdbd08fb056dbaa147b9dde1f10157b
Sushant Bansal <sushantbansal2@gmail.com>
2020-09-17 18:21:32 -0500

    Added new props

--- frontend/components/Paper/FileServer.js
+++ frontend/components/Paper/FileServer.js
@@ -5,9 +5,13 @@ import LabelValue from "../labelvalue";
 
 import { Box } from "@material-ui/core";
 
-const FileServerInfo = ({ fileserverpath }) => {
+const FileServerInfo = ({ fileserverpath, defaultOpen, editor }) => {
   return (
-    <Drawer heading="File Server Information">
+    <Drawer
+      heading="File Server Information"
+      defaultOpen={defaultOpen}
+      editor={editor}
+    >
       <Box my={1}>
         <LabelValue label="File Server Path" value={fileserverpath} />
       </Box>
@@ -17,6 +21,8 @@ const FileServerInfo = ({ fileserverpath }) => {
 
 FileServerInfo.propTypes = {
   fileserverpath: PropTypes.string.isRequired,
+  editor: PropTypes.func,
+  defaultOpen: PropTypes.bool,
 };
 
 export default FileServerInfo;
