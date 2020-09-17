24600f8bda27b9ff70b22176c87d5bf134fc2286
Sushant Bansal <sushantbansal2@gmail.com>
2020-09-17 18:23:27 -0500

    Added transitions b/w edit and view mode

--- frontend/components/CuratorElements/CuratorElement.js
+++ frontend/components/CuratorElements/CuratorElement.js
@@ -1,12 +1,13 @@
-import { useState, useContext } from "react";
-
-import { IconButton } from "@material-ui/core";
-import { Edit } from "@material-ui/icons";
+import { useState, useContext, useEffect, Fragment } from "react";
 
 import CuratorInfoForm from "../CuratorForms/CuratorInfoForm";
 import CuratorInfo from "../Paper/Curator";
 
 import CuratorContext from "../../Context/Curator/curatorContext";
+
+import ElementTransition from "../Transition/ElementTransition";
+
+import { TransitionGroup } from "react-transition-group";
 
 const CuratorElement = () => {
   const { curatorInfo } = useContext(CuratorContext);
@@ -17,15 +18,42 @@ const CuratorElement = () => {
       curatorInfo.emailIdName == ""
   );
 
-  if (editing) return <CuratorInfoForm editor={setEditing} />;
-  else
-    return (
-      <CuratorInfo
-        curator={curatorInfo}
-        editor={setEditing}
-        defaultOpen={true}
-      />
-    );
+  useEffect(() => {
+    if (
+      curatorInfo.firstName !== "" &&
+      curatorInfo.lastName !== "" &&
+      curatorInfo.emailIdName !== ""
+    ) {
+      setEditing(false);
+    }
+  }, [curatorInfo]);
+
+  return (
+    <Fragment>
+      <div className="curatorFormInfo">
+        <CuratorInfoForm editor={setEditing} />
+      </div>
+      <div className="curatorInfo">
+        <CuratorInfo
+          curator={curatorInfo}
+          editor={setEditing}
+          defaultOpen={true}
+        />
+      </div>
+      <style jsx>{`
+        .curatorFormInfo {
+          height: ${editing ? "max-content" : "0"};
+          opacity: ${editing ? 1 : 0};
+          transition: opacity 0.25s linear;
+        }
+        .curatorInfo {
+          height: ${editing ? "0" : "max-content"};
+          opacity: ${editing ? 0 : 1};
+          transition: opacity 0.25s linear;
+        }
+      `}</style>
+    </Fragment>
+  );
 };
 
 export default CuratorElement;
