3b060f1ab7daeac6c90e20c44259d8ac825623e9
Sushant Bansal <sushantbansal2@gmail.com>
2020-09-18 18:38:58 -0500

    Added new types for state management

--- frontend/Context/types.js
+++ frontend/Context/types.js
@@ -19,7 +19,8 @@ export const SET_TREE = "SET_TREE";
 export const SHOW_TREE_SELECTOR = "SHOW_TREE_SELECTOR";
 export const HIDE_TREE_SELECTOR = "HIDE_TREE_SELECTOR";
 export const SET_FILETREE_CHECKED = "SET_CHECKED";
-export const TOGGLE_MULTIPLE = "TOGGLE_MULTIPLE";
+export const SET_MULTIPLE = "SET_MULTIPLE";
+export const SET_SAVE_BUTTON_ACTION = "SAVE_BUTTON_ACTION";
 
 // Curator Actions
 export const SET_ALL = "SET_ALL";
