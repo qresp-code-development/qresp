30d9cfab1bba313b8182dc3dd64f46376066db7b
Sushant Bansal <sushantbansal2@gmail.com>
2020-09-18 18:43:35 -0500

    Added new transition component

--- frontend/components/CuratorElements/CuratorElement.js
+++ frontend/components/CuratorElements/CuratorElement.js
@@ -1,13 +1,10 @@
-import { useState, useContext, useEffect, Fragment } from "react";
+import { useState, useContext, useEffect } from "react";
 
 import CuratorInfoForm from "../CuratorForms/CuratorInfoForm";
 import CuratorInfo from "../Paper/Curator";
 
 import CuratorContext from "../../Context/Curator/curatorContext";
-
-import ElementTransition from "../Transition/ElementTransition";
-
-import { TransitionGroup } from "react-transition-group";
+import SwitchFade from "../switchFade";
 
 const CuratorElement = () => {
   const { curatorInfo } = useContext(CuratorContext);
@@ -29,30 +26,17 @@ const CuratorElement = () => {
   }, [curatorInfo]);
 
   return (
-    <Fragment>
-      <div className="curatorFormInfo">
-        <CuratorInfoForm editor={setEditing} />
-      </div>
-      <div className="curatorInfo">
+    <SwitchFade
+      editing={editing}
+      form={<CuratorInfoForm editor={setEditing} />}
+      display={
         <CuratorInfo
           curator={curatorInfo}
           editor={setEditing}
           defaultOpen={true}
         />
-      </div>
-      <style jsx>{`
-        .curatorFormInfo {
-          height: ${editing ? "max-content" : "0"};
-          opacity: ${editing ? 1 : 0};
-          transition: opacity 0.25s linear;
-        }
-        .curatorInfo {
-          height: ${editing ? "0" : "max-content"};
-          opacity: ${editing ? 0 : 1};
-          transition: opacity 0.25s linear;
-        }
-      `}</style>
-    </Fragment>
+      }
+    />
   );
 };
 
