755feb5f8c3e227e0f62a908d61f558524f36446
Sushant Bansal <sushantbansal2@gmail.com>
2020-09-22 19:21:33 -0500

    Added support to add multiple PIs

--- frontend/components/CuratorForms/PaperInfoForm.js
+++ frontend/components/CuratorForms/PaperInfoForm.js
@@ -1,12 +1,16 @@
 import PropTypes from "prop-types";
 
-import { Grid } from "@material-ui/core";
+import { Grid, Tooltip, Typography, IconButton } from "@material-ui/core";
+import {
+  AddCircleOutline,
+  RemoveCircleOutline,
+  DescriptionOutlined,
+} from "@material-ui/icons";
 
 import { TextInputField } from "../Form/InputFields";
 import { SubmitAndReset, FormInputLabel } from "../Form/Util";
 import NameInput from "../Form//NameInput";
 import Drawer from "../drawer";
-
 import { Formik, Form, FieldArray } from "formik";
 import * as Yup from "yup";
 
@@ -14,9 +18,9 @@ const PaperInfoForm = ({ editor }) => {
   const pId = {
     get: (index) => {
       return {
-        fname: `pis[${index}].firstName`,
-        mname: `pis[${index}].middleName`,
-        lname: `pis[${index}].lastName`,
+        firstName: `pis[${index}].firstName`,
+        middleName: `pis[${index}].middleName`,
+        lastName: `pis[${index}].lastName`,
       };
     },
   };
@@ -47,62 +51,142 @@ const PaperInfoForm = ({ editor }) => {
         })}
         onSubmit={(values, { setSubmitting }) => {
           setSubmitting(false);
-          alert(JSON.stringify(values));
+          alert(JSON.stringify(values, null, 2));
         }}
+        validateOnChange={false}
+        validateOnBlur={false}
+        enableReinitialize={true}
       >
-        <Form>
-          <Grid direction="column" container spacing={1}>
-            <Grid item>
-              <FieldArray
-                name="pis"
-                render={() => (
-                  <div>
-                    {values.pis.map((pi, index) => (
-                      <div key={index}>
-                        <NameInput
-                          ids={pId.get(index)}
-                          names={pId.get(index)}
-                        />
-                      </div>
-                    ))}
-                  </div>
-                )}
-              />
+        {({ values }) => (
+          <Form>
+            <Grid direction="column" container spacing={1}>
+              <FieldArray name="pis" id="pis">
+                {({ remove, push }) => {
+                  return (
+                    <Grid item>
+                      <Grid
+                        container
+                        spacing={2}
+                        justify="flex-start"
+                        alignItems="center"
+                      >
+                        <Grid item>
+                          <FormInputLabel
+                            label="Principal Investigators"
+                            forId="pis"
+                          />
+                        </Grid>
+                        <Grid item>
+                          <Tooltip
+                            title={
+                              <Typography variant="subtitle2">
+                                Add a principle investigator
+                              </Typography>
+                            }
+                            placement="right"
+                            arrow
+                          >
+                            <IconButton
+                              onClick={() =>
+                                push({
+                                  firstName: "",
+                                  middleName: "",
+                                  lastName: "",
+                                })
+                              }
+                              style={{ padding: 0 }}
+                            >
+                              <AddCircleOutline color="primary" />
+                            </IconButton>
+                          </Tooltip>
+                        </Grid>
+                      </Grid>
+                      {values.pis.map((pi, index) => {
+                        return (
+                          <NameInput
+                            ids={pId.get(index)}
+                            names={pId.get(index)}
+                            key={index}
+                            id={`pi${index}`}
+                            remove={
+                              <Tooltip
+                                title={
+                                  <Typography variant="subtitle2">
+                                    {values.pis.length == 1
+                                      ? "Required (minimum one P.I.)"
+                                      : "Remove principle investigator"}
+                                  </Typography>
+                                }
+                                placement="right"
+                                arrow
+                              >
+                                <IconButton
+                                  size="small"
+                                  onClick={() => {
+                                    if (values.pis.length > 1) {
+                                      remove(index);
+                                    }
+                                  }}
+                                  style={{ padding: 0 }}
+                                >
+                                  <RemoveCircleOutline
+                                    color={
+                                      values.pis.length == 1
+                                        ? "disabled"
+                                        : "primary"
+                                    }
+                                  />
+                                </IconButton>
+                              </Tooltip>
+                            }
+                          />
+                        );
+                      })}
+                    </Grid>
+                  );
+                }}
+              </FieldArray>
+
+              <Grid item>
+                <TextInputField
+                  id="paperstack"
+                  placeholder="Enter collection to which project belongs to"
+                  name="paperStack"
+                  helperText="Enter names(s) defining group of papers (eg. according to the source of fundings)"
+                  label="PaperStack"
+                  required
+                />
+              </Grid>
+              <Grid item>
+                <TextInputField
+                  id="tags"
+                  placeholder="Ener tags for the project"
+                  name="tags"
+                  helperText="Enter keywords(s) (e.g. DFT, oragnic materials, charge transfer)"
+                  label="Keywords"
+                  required
+                />
+              </Grid>
+              <Grid item>
+                <TextInputField
+                  id="mainNotebookFile"
+                  placeholder="Enter main notebook filename"
+                  name="mainNotebookFile"
+                  helperText="Enter name of a notebook file, thid file may serve as a table of contents and may contain links to all datasets, charts, scripts, tools and documentation. Use the file picker button to fill this field. "
+                  label="Main Notebook File"
+                  action={
+                    <IconButton size="small">
+                      <DescriptionOutlined color="primary" />
+                    </IconButton>
+                  }
+                />
+              </Grid>
+              <Grid item>
+                <SubmitAndReset submitText="Save" />
+              </Grid>
             </Grid>
-            <Grid item>
-              <TextInputField
-                id="paperstack"
-                placeholder="Enter collection to which project belongs to"
-                name="paperStack"
-                helperText="Enter names(s) defining group of papers (eg. according to the source of fundings)"
-                label="PaperStack"
-                required
-              />
-            </Grid>
-            <Grid item>
-              <TextInputField
-                id="tags"
-                placeholder="Ener tags for the project"
-                name="tags"
-                helperText="Enter keywords(s) (e.g. DFT, oragnic materials, charge transfer)"
-                label="Keywords"
-                required
-              />
-            </Grid>
-            <Grid item>
-              <TextInputField
-                id="mainNotebookFile"
-                placeholder="Enter main notebook filename"
-                name="mainNotebookFile"
-                helperText="Enter name of a notebook file, thid file may serve as a table of contents and may contain links to all datasets, charts, scripts, tools and documentation. Use the file picker button to fill this field. "
-                label="Main Notebook File"
-              />
-            </Grid>
-            <Grid item>
-              <SubmitAndReset submitText="Save" />
-            </Grid>
-          </Grid>
-        </Form>
+          </Form>
+        )}
       </Formik>
     </Drawer>
   );
