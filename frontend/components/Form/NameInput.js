0c5fe0c852c892cec52010fa14dbffa165480bca
Sushant Bansal <sushantbansal2@gmail.com>
2020-09-22 19:17:42 -0500

    Added support for action button with name input

--- frontend/components/Form/NameInput.js
+++ frontend/components/Form/NameInput.js
@@ -3,53 +3,65 @@ import PropTypes from "prop-types";
 import { Grid } from "@material-ui/core";
 import TextInput from "./TextInput";
 
-const NameInput = ({ ids, names }) => {
+const NameInput = ({ ids, names, remove, id }) => {
+  const width = 4;
   return (
-    <Grid container direction="row" spacing={2} justify="space-between">
-      <Grid item xs={12} sm={4}>
+    <Grid container direction="row" spacing={2} justify="space-around" id={id}>
+      <Grid item xs={12} sm={width}>
         <TextInput
-          id={ids.fname}
+          id={ids.firstName}
+          label="First Name"
           placeholder="Enter first name"
           name={names.firstName}
           helperText="eg. Jane"
         />
       </Grid>
-      <Grid item xs={12} sm={4}>
+      <Grid item xs={12} sm={remove ? width - 1 : width}>
         <TextInput
-          id={ids.mname}
+          id={ids.middleName}
+          label="Middle Name"
           placeholder="Enter middle name"
           name={names.middleName}
           helperText="eg. L."
         />
       </Grid>
-      <Grid item xs={12} sm={4}>
+      <Grid item xs={12} sm={width}>
         <TextInput
-          id={ids.lname}
+          id={ids.lastName}
+          label="Last Name"
           placeholder="Enter last name"
           name={names.lastName}
           helperText="eg. Doe"
         />
       </Grid>
+      {remove ? (
+        <Grid item item xs={12} sm={1} style={{ margin: "auto" }}>
+          {remove}
+        </Grid>
+      ) : null}
     </Grid>
   );
 };
 
 NameInput.defaultProps = {
   ids: {
-    fname: "firstName",
-    mname: "middleName",
-    lname: "lastName",
+    firstName: "firstName",
+    middleName: "middleName",
+    lastName: "lastName",
   },
   names: {
     firstName: "firstName",
     middleName: "middleName",
     lastName: "lastName",
   },
+  remove: null,
 };
 
 NameInput.propTypes = {
   ids: PropTypes.object,
+  id: PropTypes.string.isRequired,
   names: PropTypes.object,
+  remove: PropTypes.object,
 };
 
 export default NameInput;
