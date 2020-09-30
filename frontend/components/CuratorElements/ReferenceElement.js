e1dfe5bf473e53091e80707c49d8c3f548018125
Sushant Bansal <sushantbansal2@gmail.com>
2020-09-29 19:59:56 -0500

    Finished

--- frontend/components/CuratorElements/ReferenceElement.js
+++ frontend/components/CuratorElements/ReferenceElement.js
@@ -1,28 +1,33 @@
 import { useState, useContext, useEffect } from "react";
 
 import ReferenceInfoForm from "../CuratorForms/ReferenceInfoForm";
-// import PaperInfo from "../Paper/Info";
+import ReferenceC from "../Paper/ReferenceC";
 
 import CuratorContext from "../../Context/Curator/curatorContext";
 import SwitchFade from "../switchFade";
 
 const ReferenceInfoElement = () => {
-  //   const { paperInfo } = useContext(CuratorContext);
+  const { referenceInfo } = useContext(CuratorContext);
 
-    const [editing, setEditing] = useState(true);
+  const [editing, setEditing] = useState(true);
 
-  //     useEffect(() => {
-  //       if (paperInfo.tags.length > 0 ) {
-  //         setEditing(false);
-  //       }
-  //     }, [paperInfo]);
+  useEffect(() => {
+    if (referenceInfo.title) {
+      setEditing(false);
+    }
+  }, [referenceInfo]);
 
   return (
     <SwitchFade
       editing={editing}
       form={<ReferenceInfoForm editor={setEditing} />}
-      display={<div>Ok</div>}
-      //   display={<PaperInfo paperInfo={paperInfo} editor={setEditing} defaultOpen={true}/>}
+      display={
+        <ReferenceC
+          referenceInfo={referenceInfo}
+          editor={setEditing}
+          defaultOpen={true}
+        />
+      }
     />
   );
 };
