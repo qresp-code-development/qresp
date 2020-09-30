1cde752ad0685c4a05b1ece419b9ddd15275f057
Sushant Bansal <sushantbansal2@gmail.com>
2020-09-29 19:57:46 -0500

    Fixed bugs in reference and names utilities

--- frontend/Utils/utils.js
+++ frontend/Utils/utils.js
@@ -11,11 +11,14 @@ const namesUtil = {
     const result = names.split(",").map((el) => {
       const splits = el.trim().split(" ");
       const obj = { firstName: splits[0], middleName: "", lastName: "" };
-      if (splits.length === 3) {
-        obj.middleName = splits[1];
-        obj.lastName = splits[2];
+      if (splits.length >= 3) {
+        obj.middleName = splits
+          .slice(1, splits.length - 1)
+          .join(" ")
+          .trim();
+        obj.lastName = splits[splits.length - 1].trim();
       } else {
-        obj.lastName = splits[1];
+        obj.lastName = splits[1].trim();
       }
       return obj;
     });
@@ -26,7 +29,7 @@ const namesUtil = {
     const result = names.map((name) => {
       const n = [];
       Object.keys(name).forEach((key) => {
-        n.push(name[key]);
+        n.push(name[key].trim());
       });
       return n.join(" ");
     });
@@ -35,7 +38,7 @@ const namesUtil = {
 };
 
 const referenceUtil = {
-  set: (journal, year, page, volume) =>
+  set: ({ journal, year, page, volume }) =>
     `${journal} ${year}, ${volume} ,${page}`,
 
   get: (text) => {
@@ -43,11 +46,10 @@ const referenceUtil = {
     if (!text) return values;
     const split1 = text.split(",");
     const split2 = split1[0].split(" ");
-    values.journal = split2[0];
-    values.year = split2[1];
-    values.page = split1[3];
-    values.volume = split1[2];
-
+    values.journal = split2.slice(0, split2.length - 1).join(" ");
+    values.year = split2[split2.length - 1];
+    values.page = split1[2];
+    values.volume = split1[1];
     return values;
   },
 };
