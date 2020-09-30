710464b71cbc9462a71b1376e4ac4fc9656622fb
Sushant Bansal <sushantbansal2@gmail.com>
2020-09-30 13:47:09 -0500

    Added pretty urls, fixed set children method

--- frontend/Utils/Scraper.js
+++ frontend/Utils/Scraper.js
@@ -1,13 +1,12 @@
-import apiEndpoint from "../Context/axios";
+import axios from "axios";
 
 const node = (label, value, folder) => {
   const node = {
     label: label,
-    value: value,
-    folder: folder,
+    value: value.endsWith("/") ? value.slice(0, value.length - 1) : value,
   };
   if (folder) {
-    node["children"] = null;
+    node["children"] = [];
   }
   return node;
 };
@@ -19,7 +18,7 @@ const getList = async (url, type, service) => {
   );
 
   try {
-    const response = await apiEndpoint.post(
+    const response = await axios.post(
       server + "/api/dircont",
       JSON.stringify({ link: url, src: type, service: service }),
       {
@@ -41,25 +40,17 @@ const getList = async (url, type, service) => {
   }
 };
 
-const getStructure = async (url, type, service) => {
-  const list = await getList(url, type, service);
-  const details = list.details;
-
-  const subFolders = await Promise.all(
-    list.files.map((el) => {
-      if (el.folder) {
-        const data = getList(el.value, type, false).then((data) => data.files);
-        return data;
-      }
-      return [];
-    })
-  );
-
-  list.files.forEach((element, i) => {
-    element.children = subFolders[i];
+const findAndSetChildren = (state, parentValue, children) => {
+  state.forEach((node) => {
+    if (node.value == parentValue) {
+      node.children = [...children];
+      return;
+    } else {
+      if (node.children && node.children.length > 0)
+        findAndSetChildren(node.children, parentValue, children);
+    }
   });
-
-  return { files: list.files, details };
+  return state;
 };
 
-export { getList, getStructure };
+export { getList, findAndSetChildren };
