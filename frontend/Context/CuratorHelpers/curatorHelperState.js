2b948268d4bcc0eb4205f2a6da04405b68855d08
Sushant Bansal <sushantbansal2@gmail.com>
2020-10-02 17:40:43 -0500

    Made the helper context very reusable and modular

--- frontend/Context/CuratorHelpers/curatorHelperState.js
+++ frontend/Context/CuratorHelpers/curatorHelperState.js
@@ -1,28 +1,32 @@
-import React, { useReducer, useEffect } from "react";
+import React, { useReducer } from "react";
 import CuratorHelperContext from "./curatorHelperContext";
 import curatorHelperReducer from "./curatorHelperReducer";
 
-import { OPEN_CHART_FORM, CLOSE_CHART_FORM, SET_CHART_DEF } from "../types";
+import { OPEN_FORM, CLOSE_FORM, SET_DEF } from "../types";
 
 const CuratorHelperState = (props) => {
   const initialState = {
-    chartsHelper: { def: null, open: false },
+    open: { chart: false, tool: false, dataset: false, script: false },
+    def: { chart: null, tool: null, dataset: null, script: null },
   };
 
   const [state, dispatch] = useReducer(curatorHelperReducer, initialState);
 
-  const openChartForm = () => dispatch({ type: OPEN_CHART_FORM });
-  const closeChartForm = () => dispatch({ type: CLOSE_CHART_FORM });
-  const setDefaultChart = (def) =>
-    dispatch({ type: SET_CHART_DEF, payload: def });
+  const openForm = (type) => dispatch({ type: OPEN_FORM, payload: type });
+  const closeForm = (type) => dispatch({ type: CLOSE_FORM, payload: type });
+  const setDefault = (type, def) =>
+    dispatch({ type: SET_DEF, payload: { type: type, value: def } });
 
   return (
     <CuratorHelperContext.Provider
       value={{
-        chartsHelper: state.chartsHelper,
-        openChartForm,
-        closeChartForm,
-        setDefaultChart,
+        chartsHelper: { open: state.open.chart, def: state.def.chart },
+        toolsHelper: { open: state.open.tool, def: state.def.tool },
+        datasetsHelper: { open: state.open.dataset, def: state.def.dataset },
+        scriptsHelper: { open: state.open.script, def: state.def.script },
+        openForm,
+        closeForm,
+        setDefault,
       }}
     >
       {props.children}
