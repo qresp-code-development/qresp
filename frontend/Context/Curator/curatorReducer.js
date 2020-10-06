2322888c379b3befa93da17c2755a8a890d4b118
Sushant Bansal <sushantbansal2@gmail.com>
2020-10-05 19:11:16 -0500

    Added new workflow methods and reducers

--- frontend/Context/Curator/curatorReducer.js
+++ frontend/Context/Curator/curatorReducer.js
@@ -9,6 +9,12 @@ import {
   ADD,
   EDIT,
   DELETE,
+  ADD_NODE,
+  ADD_EDGE,
+  DELETE_EDGE,
+  DELETE_NODE,
+  SET_NODES,
+  SET_EDGES,
 } from "../types";
 
 export default (state, action) => {
@@ -42,21 +48,8 @@ export default (state, action) => {
         ],
       };
     case DELETE:
-      var prefix;
-      switch (action.payload.type) {
-        case "charts":
-          prefix = "c";
-          break;
-        case "tools":
-          prefix = "t";
-          break;
-        case "scripts":
-          prefix = "s";
-          break;
-        case "datasets":
-          prefix = "d";
-          break;
-      }
+      const prefix = action.payload.type.charAt(0);
+
       return {
         ...state,
         [action.payload.type]: state[action.payload.type]
@@ -69,6 +62,56 @@ export default (state, action) => {
         [action.payload.type]: state[action.payload.type].map((el) =>
           el.id == action.payload.value.id ? action.payload.value : el
         ),
+      };
+
+    case SET_NODES:
+      return {
+        ...state,
+        workflow: { ...state.workflow, nodes: [...action.payload] },
+      };
+    case SET_EDGES:
+      return {
+        ...state,
+        workflow: { ...state.workflow, edges: [...action.payload] },
+      };
+    case ADD_NODE:
+      return {
+        ...state,
+        workflow: {
+          ...state.workflow,
+          nodes: [...state.workflow.nodes, ...action.payload],
+        },
+      };
+    case ADD_EDGE:
+      return {
+        ...state,
+        workflow: {
+          ...state.workflow,
+          edges: [...state.workflow.edges, ...action.payload],
+        },
+      };
+    case DELETE_NODE:
+      return {
+        ...state,
+        workflow: {
+          ...state.workflow,
+          nodes: state.workflow.nodes.filter((node) => node != action.payload),
+          // Deleting a node also deletes any edges associated to it
+          edges: state.workflow.edges.filter(
+            (edge) => edge[0] != action.payload && edge[1] != action.payload
+          ),
+        },
+      };
+    case DELETE_EDGE:
+      return {
+        ...state,
+        workflow: {
+          ...state.workflow,
+          edges: state.workflow.edges.filter(
+            (edge) =>
+              edge[0] != action.payload[0] && edge[1] != action.payload[1]
+          ),
+        },
       };
     default:
       return state;
