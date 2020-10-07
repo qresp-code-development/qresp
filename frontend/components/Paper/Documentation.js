b762437e44714ab607b1db4898bdf13fdf1f653b
Sushant Bansal <sushantbansal2@gmail.com>
2020-10-07 13:00:39 -0500

    Added new props to Documentation to make it more controllable

--- frontend/components/Paper/Documentation.js
+++ frontend/components/Paper/Documentation.js
@@ -4,9 +4,9 @@ import Drawer from "../drawer";
 
 import { Typography, Box } from "@material-ui/core";
 
-const Documentation = ({ documentation }) => {
+const Documentation = ({ documentation, editor, defaultOpen }) => {
   return (
-    <Drawer heading="Documentation">
+    <Drawer heading="Documentation" editor={editor} defaultOpen={defaultOpen}>
       <Box my={1}>
         <Typography variant="h6" component="div" color="secondary">
           <Box fontWeight="bold">Readme</Box>
@@ -19,6 +19,8 @@ const Documentation = ({ documentation }) => {
 
 Documentation.propTypes = {
   documentation: PropTypes.string.isRequired,
+  editor: PropTypes.func,
+  defaultOpen: PropTypes.bool,
 };
 
 export default Documentation;
