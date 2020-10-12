a6cd19be9ced9e170fdb77dda5bfd7c37689e39b
Sushant Bansal <sushantbansal2@gmail.com>
2020-10-12 18:10:48 -0500

    Moved static data to seperate folder

---
+++ frontend/data/licenses.js
@@ -0,0 +1,43 @@
+const licenses = {
+  cco: {
+    title: "Creative Commons Zero v1.0 Universal",
+    link: "https://creativecommons.org/publicdomain/zero/1.0/",
+    infographics: ["cc.svg", "zero.svg"],
+  },
+  cc_by: {
+    title: "Creative Commons Attribution 4.0 International License",
+    link: "https://creativecommons.org/licenses/by/4.0/",
+    infographics: ["cc.svg", "by.svg"],
+  },
+  cc_by_nc: {
+    title:
+      "Creative Commons Attribution NonCommercial 4.0 International License",
+    link: "https://creativecommons.org/licenses/by-nc/4.0/",
+    infographics: ["cc.svg", "by.svg", "nc.svg"],
+  },
+  cc_by_sa: {
+    title: "Creative Commons Attribution ShareAlike 4.0 International License",
+    link: "https://creativecommons.org/licenses/by-sa/4.0/",
+    infographics: ["cc.svg", "by.svg", "sa.svg"],
+  },
+  cc_by_nd: {
+    title:
+      "Creative Commons Attribution NoDerivatives 4.0 International License",
+    link: "https://creativecommons.org/licenses/by-nd/4.0/",
+    infographics: ["cc.svg", "by.svg", "nd.svg"],
+  },
+  cc_by_nc_sa: {
+    title:
+      "Creative Commons Attribution NonCommercial ShareAlike 4.0 International License",
+    link: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+    infographics: ["cc.svg", "by.svg", "nc.svg", "sa.svg"],
+  },
+  cc_by_nc_nd: {
+    title:
+      "Creative Commons Attribution NonCommercial NoDerivatives 4.0 International License",
+    link: "https://creativecommons.org/licenses/by-nc-nd/4.0/",
+    infographics: ["cc.svg", "by.svg", "nc.svg", "nd.svg"],
+  },
+};
+
+export default licenses;
