d93f9e5bbccd826c18d85ba19cf60fc26439f8f9
Sushant Bansal <sushantbansal2@gmail.com>
2020-10-20 18:34:35 -0500

    Added CC License abbreviations

--- frontend/data/licenses.js
+++ frontend/data/licenses.js
@@ -1,40 +1,41 @@
 const licenses = {
   cco: {
-    title: "Creative Commons Zero v1.0 Universal",
+    title: "Creative Commons Zero v1.0 Universal (CCO)",
     link: "https://creativecommons.org/publicdomain/zero/1.0/",
     infographics: ["cc.svg", "zero.svg"],
   },
   cc_by: {
-    title: "Creative Commons Attribution 4.0 International License",
+    title: "Creative Commons Attribution 4.0 International License (CC BY)",
     link: "https://creativecommons.org/licenses/by/4.0/",
     infographics: ["cc.svg", "by.svg"],
   },
   cc_by_nc: {
     title:
-      "Creative Commons Attribution NonCommercial 4.0 International License",
+      "Creative Commons Attribution NonCommercial 4.0 International License (CC BY-NC)",
     link: "https://creativecommons.org/licenses/by-nc/4.0/",
     infographics: ["cc.svg", "by.svg", "nc.svg"],
   },
   cc_by_sa: {
-    title: "Creative Commons Attribution ShareAlike 4.0 International License",
+    title:
+      "Creative Commons Attribution ShareAlike 4.0 International License (CC BY-SA)",
     link: "https://creativecommons.org/licenses/by-sa/4.0/",
     infographics: ["cc.svg", "by.svg", "sa.svg"],
   },
   cc_by_nd: {
     title:
-      "Creative Commons Attribution NoDerivatives 4.0 International License",
+      "Creative Commons Attribution NoDerivatives 4.0 International License (CC BY-ND)",
     link: "https://creativecommons.org/licenses/by-nd/4.0/",
     infographics: ["cc.svg", "by.svg", "nd.svg"],
   },
   cc_by_nc_sa: {
     title:
-      "Creative Commons Attribution NonCommercial ShareAlike 4.0 International License",
+      "Creative Commons Attribution NonCommercial ShareAlike 4.0 International License (CC BY-NC-SA)",
     link: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
     infographics: ["cc.svg", "by.svg", "nc.svg", "sa.svg"],
   },
   cc_by_nc_nd: {
     title:
-      "Creative Commons Attribution NonCommercial NoDerivatives 4.0 International License",
+      "Creative Commons Attribution NonCommercial NoDerivatives 4.0 International License (CC BY-NC-ND)",
     link: "https://creativecommons.org/licenses/by-nc-nd/4.0/",
     infographics: ["cc.svg", "by.svg", "nc.svg", "nd.svg"],
   },
