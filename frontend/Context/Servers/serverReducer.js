5a01732404cfb0a87eb9cd21df6673618dd561b3
Sushant Bansal <sushantbansal2@gmail.com>
2020-11-19 17:55:26 -0600

    Added server state storage to browser, fix for broken download paths

--- frontend/Context/Servers/serverReducer.js
+++ frontend/Context/Servers/serverReducer.js
@@ -1,7 +1,9 @@
-import { SET_SELECTED, SET_SELECTED_HTTP } from "../types";
+import { SET_SELECTED, SET_SELECTED_HTTP, SET_SERVER_STATE } from "../types";
 
 export default (state, action) => {
   switch (action.type) {
+    case SET_SERVER_STATE:
+      return action.payload;
     case SET_SELECTED:
       return { ...state, selected: action.payload };
     case SET_SELECTED_HTTP:
